7ab90e62cff9eaf75df4dbeb42846daee9f75058
chiaradeleo1 <chiara.deleo@students.uniroma2.eu>
2024-09-13 21:42:35 +0200

    added switch

--- source_code/.ipynb_checkpoints/fisher_for_eft-checkpoint.py
+++ source_code/.ipynb_checkpoints/fisher_for_eft-checkpoint.py
@@ -21,17 +21,21 @@ class get_Fisher:
         self. param_values = [self.obs_settings['extra'][name] if name in self.obs_settings['extra'] else self.fiducial[name] for name in self.param_names]
         self.param_deltas = [free_params[name] for name in self.param_names]
         self.obs=[]
-        
+        self.numbins=0
         if 'GC' in self.observables:
             self.Nbins_gc=self.observables['GC']['Nbins']
             self.obs.append('G')
-            
+            self.numbins=max(self.Nbins_gc, self.numbins)
         if 'WL' in self.observables:
             self.Nbins_wl=self.observables['WL']['Nbins']
             self.obs.append('L')
-            
-        self.numbins=max(self.Nbins_gc, self.Nbins_wl)
+            self.numbins=max(self.Nbins_wl, self.numbins)
+        if 'GW' in self.observables:
+            self.Nbins_gw=self.observables['GW']['Nbins']
+            self.obs.append('W')
+            self.numbins=max(self.numbins,  self.Nbins_gw)
         self.cols = [f'{o}{ind+1}' for o in self.obs for ind in range(self.numbins)]
+       
 
 
     def numerical_derivative(self):
@@ -56,8 +60,9 @@ class get_Fisher:
                 fiducial_plus[param] += epsilon
                 fiducial_minus[param] -= epsilon
             #print(param,fiducial_plus, fiducial_minus,extra_plus , extra_minus)                
-            Cls_plus = get_obs(fiducial_plus, extra_plus, self.observables, self.ells, self.obs_settings['camb_path'], self.obs_settings['case'], feedback=False).Cls
-            Cls_minus = get_obs(fiducial_minus, extra_minus, self.observables, self.ells, self.obs_settings['camb_path'],self.obs_settings['case'], feedback=False).Cls
+            Cls_plus = get_obs(fiducial_plus, extra_plus, self.observables, self.ells, self.obs_settings['camb_path'], self.obs_settings['case'],self.obs_settings['source'], feedback=False).Cls
+            Cls_minus = get_obs(fiducial_minus, extra_minus, self.observables, self.ells, self.obs_settings['camb_path'],self.obs_settings['case'],self.obs_settings['source'], feedback=False).Cls
+            
             for ia, aa in enumerate(self.cols):
                 for ib, bb in enumerate(self.cols):
                     if ia>ib:
@@ -78,9 +83,18 @@ class get_Fisher:
 
 
 
+
+
     def get_cls_noisy(self):
+        
+        if 'GW' in self.observables:
+            GW_specs = {'fsky': 0.35, 
+                'N_gw': 10**5, 
+                'sigma_eps_gw': 0.005}
+            ngwbin = GW_specs['N_gw']/self.Nbins_gw ######SISTEMA
+            
         ngalbin = (self.galaxy_specs['gal_per_arcmin']/self.numbins)*3600*(180/np.pi)**2
-        calc_obs= get_obs(self.fiducial,self.obs_settings['extra'], self.observables, self.ells, self.obs_settings['camb_path'], self.obs_settings['case'], feedback=False)
+        calc_obs= get_obs(self.fiducial,self.obs_settings['extra'], self.observables, self.ells, self.obs_settings['camb_path'], self.obs_settings['case'], self.obs_settings['source'],feedback=False)
         eps_error = self.galaxy_specs['sigma_eps']
         noisy_cls =  copy.deepcopy(calc_obs.Cls)
         #print(calc_obs.Cls)
@@ -88,10 +102,14 @@ class get_Fisher:
     
         for ind in (1,self.numbins):
             for obs in self.obs:
-                if obs == 'GC':
+                print(self.obs)
+                if obs == 'G':
                     noisy_cls['G'+str(ind)+'xG'+str(ind)] += 1/ngalbin
-                if obs == 'WL':
+                if obs == 'L':
                     noisy_cls['L'+str(ind)+'xL'+str(ind)] += (eps_error**2)/(2*ngalbin )
+                if obs == 'W':
+                    noisy_cls['W'+str(ind)+'xW'+str(ind)] += [(GW_specs['sigma_eps_gw']**2/ngwbin)]
+                    print('Hello')
         return noisy_cls
 
 
--- source_code/compute_obs_sources.py
+++ source_code/compute_obs_sources.py
@@ -14,7 +14,7 @@ possible_observables = ['GC','WL','GW']
 
 class get_obs:
 
-    def __init__(self,params, extra, observables,ells,camb_path,case,feedback=False):
+    def __init__(self,params, extra, observables,ells,camb_path,case,source,feedback=False):
 
         #Reading used observables from source distribution
         #Checking that there is no weird stuff
@@ -24,6 +24,7 @@ class get_obs:
         self.case=case
         self.camb_path=camb_path
         self.ells=ells
+        self.source=source
         self.zinterps = np.logspace(-3,np.log10(5),500)
         self.k_max_Boltzmann = 10
         
@@ -39,15 +40,23 @@ class get_obs:
         
 
         tini = time()
-        self.Cls = self.get_cls( ells)
+        if self.source==True:
+            self.Cls = self.get_cls( ells)
+        else:
+            self.Cls =self.get_cls_old()
         tend = time()
     
         if feedback:
             print('Cls computed in {:.1f} s'.format(tend - tini))
 
+
+
+
+    
+
     def get_cosmo_dict(self,params): 
                 
-        nuis_keys  = ['_derived','a0','a1', 'a2', 'a3', 'a4', 'b0_poly','b1_poly','b2_poly','b3_poly', 'logA', 'omegam', 'omegab', 'sigma8']
+        nuis_keys  = ['_derived','a0','a1', 'a2', 'a3', 'a4', 'b0_poly', 'b1_poly', 'b2_poly', 'b3_poly', 'logA', 'omegam', 'omegab', 'sigma8']
         nuis_keys = nuis_keys + list(self.extra_params.keys())
         
         cosmo_params = {par: params[par] for par in params.keys() if par not in nuis_keys}
@@ -64,20 +73,74 @@ class get_obs:
 
         bgrid = [sum([params['b{}_poly'.format(ind)]*np.power(z,ind) for ind in range(4)]) for z in self.zinterps]
         cosmo_dict['bias'] = interp1d(self.zinterps,bgrid)
-        
+
+        if self.source==False:
+            cosmo=self.additional_cosmo_dict(cosmo_dict)  
+            cosmo_dict=cosmo
+        #print(cosmo_dict)
         return cosmo_dict
+
+
+    def additional_cosmo_dict(self,cosmo_dict):
+        sys.path.insert(0,self.camb_path)
+        
+        self.k_max_extrap= 1000000.0
+        self.k_min_extrap= 0.00001
+        import camb
+        from   camb         import model
+        
+        cosmo_pars = cosmo_dict['cosmo_params']
+        #print(cosmo_pars)
+        self.z_camb=np.linspace(0.001,4.,100)
+        if 'eft_params' in cosmo_dict:
+            eft_params=cosmo_dict['eft_params']
+            pars = camb.set_params(**cosmo_pars,**eft_params)
+            
+
+        else:
+             pars = camb.set_params(**cosmo_pars)
+            
+        pars.NonLinear = model.NonLinear_both
+        results = camb.get_results(pars)
+        Omega0_cdm=results.get_Omega('cdm', z=0)
+        Omega0_b=results.get_Omega('baryon', z=0)
+        cosmo_dict['z']=self.z_camb
+        cosmo_dict['Omm']= Omega0_cdm+Omega0_b
+        cosmo_dict['comov_dist']=interp1d(self.z_camb,results.comoving_radial_distance(self.z_camb))
+        cosmo_dict['angular_dist']=interp1d(self.z_camb,results.angular_diameter_distance(self.z_camb))
+        cosmo_dict['H_Mpc'] = interp1d(self.z_camb, results.h_of_z(self.z_camb))
+
+        cosmo_dict['Pk_linear'] = camb.get_matter_power_interpolator(pars, nonlinear=False, 
+    hubble_units=False, k_hunit=False, kmax=10, var1='delta_tot',var2='delta_tot', zmax=self.z_camb[-1])
+   
+
+
+        cosmo_dict['Pk_delta']= camb.get_matter_power_interpolator(pars, nonlinear=True, 
+    hubble_units=False, k_hunit=False, kmax=10, var1='delta_tot',var2='delta_tot', zmax=self.z_camb[-1])
+        cosmo_dict['Pk_Weyl']=camb.get_matter_power_interpolator(pars, nonlinear=False, 
+    hubble_units=False, k_hunit=False, kmax=10, var1='Weyl',var2='Weyl', zmax=self.z_camb[-1])
+    
+
+
+
+        ks = 0.001
+        P_z_k = cosmo_dict['Pk_delta'].P(self.z_camb, ks)
+        cosmo_dict['Dz'] = np.sqrt(P_z_k / cosmo_dict['Pk_delta'].P(0.001, ks))
+        return cosmo_dict
+    
 
   
     def get_source_Cls(self,cosmo):
         
         sys.path.insert(0,self.camb_path)
-        
+         
         import camb
         from   camb.sources import GaussianSourceWindow, SplinedSourceWindow
         from   camb         import model, initialpower
         self.z            = np.linspace(0.001,4,500)
+        
         cosmo_pars = cosmo['cosmo_params']
-        print(camb.__path__)
+        #print(cosmo_pars)
         if 'eft_params' in cosmo:
             eft_params=cosmo['eft_params']
             #print(eft_params)
@@ -90,7 +153,7 @@ class get_obs:
         pars.NonLinear = model.NonLinear_both
         use_obs     = []
         window_list = []
-
+        #print(pars)
         if 'GC' in self.observables:
             Nbins_GC    = self.observables['GC']['Nbins']
             n_dict      = {i+1: self.observables['GC']['dist'][i](self.z) for i in range(0, Nbins_GC)}
@@ -114,15 +177,8 @@ class get_obs:
             use_obs.append('WL')
 
         if 'GW' in self.observables:
-            Nbins_GW    = self.observables['GW']['Nbins']
-            n_dict      = {i+1: self.observables['GW']['dist'][i](self.z) for i in range(0, Nbins_GC)}
-            bias_binned = cosmo['bias'](self.observables['GW']['zmean'])
-            print(bias_binned)
-            window_GW=[SplinedSourceWindow(source_type='gws', bias=bias_binned[i-1], z=self.z, W=n_dict[i]) 
-                                       for i in range(1, Nbins_GW+1)]
-            print(window_GW)
-            window_list = window_list+[SplinedSourceWindow(source_type='gws', bias=bias_binned[i-1], z=self.z, W=n_dict[i]) 
-                                       for i in range(1, Nbins_GW+1)]
+            sys.exit('GW not implemented yet!')
+            use_obs.append('GW')
 
         pars.SourceWindows = window_list
 
@@ -131,12 +187,17 @@ class get_obs:
         results = camb.get_results(pars)
 
         cls = results.get_source_cls_dict(lmax=max(self.ells), raw_cl=True)
-        
-        #print(cls.keys())
         if self.feedback:
             print('CAMB took {:.2f} s'.format(time()-tini))
 
         return cls
+
+
+
+
+
+
+    
 
 
     def set_camb_specs(self,pars,case):#='simple'):
@@ -212,6 +273,14 @@ class get_obs:
             sys.exit('UNKNOWN CAMB CASE {}'.format(case))
 
         return pars
+
+
+
+
+
+
+
+    
         
     def get_cls(self,ells):
         cosmo=self.get_cosmo_dict(self.params)
@@ -241,7 +310,7 @@ class get_obs:
             GGLcols = ['G{}xL{}'.format(i,j) for i in range(1,self.observables['GC']['Nbins']+1) for j in range(1,self.observables['WL']['Nbins']+1)]
 
         if 'GW' in self.observables:
-            #sys.exit('GW not implemented yet!')
+            sys.exit('GW not implemented yet!')
             use_obs.append('GW')
 
         
@@ -260,10 +329,6 @@ class get_obs:
 
         final_Cls = pd.DataFrame(columns=['ells']+all_cols)
         final_Cls['ells'] = ells
-        #Nbin={'GC': Nbins_GC+1,
-         #     'WL': Nbins_GC+Nbins_WL+1,
-          #    'IA': Nbins_GC+(2*Nbins_WL)+1}
-
         
         tini = time()
         if 'GC' in use_obs:
@@ -345,5 +410,218 @@ class get_obs:
             print('Cls packing took {:.2f} s'.format(time()-tini))
         return final_Cls
 
-   
 
+
+
+
+
+    
+
+    def get_window(self):
+        self.kernels = {}
+        cosmo=self.get_cosmo_dict(self.params)
+        #self.Nbins=self.distribution['GC']['Nbins']
+        self.z            = np.linspace(0.001,4,500)
+        
+        
+        # Computing kernels for different observations
+        tini = time()
+    
+        if 'GC' in self.observables:
+            self.Nbins_GC    = self.observables['GC']['Nbins']
+            self.ni_GC     = self.observables['GC']['dist']
+            self.kernels.update({'g'+str(i+1): self.gal_window(cosmo, i) for i in range(0, len(self.ni_GC))})
+        
+        if 'WL' in self.observables:
+            self.Nbins_WL  = self.observables['WL']['Nbins']
+            self.ni_WL    = self.observables['WL']['dist']
+            self.leff = [np.array([self.lens_eff(cosmo, z, i) for z in cosmo['z']]) for i in range(len(self.ni_WL))]
+            self.kernels.update({'d'+str(i+1): self.lens_window(cosmo, i) for i in range(0,len(self.ni_WL))})
+            self.kernels.update({'i'+str(i+1): self.IA_window(cosmo, i) for i in range(0,len(self.ni_WL))})
+        
+        if 'GW' in self.observables:
+            self.Nbins_GW    = self.observables['GW']['Nbins']
+            self.ni_GW     = self.observables['GW']['dist']
+            self.leff_gw = [np.array([self.gw_eff(cosmo, z, i) for z in cosmo['z']]) for i in range(len(self.ni_GW))]
+            self.kernels.update({'gw'+str(i+1): self.gw_window(cosmo, i) for i in range(len(self.ni_GW))})
+        tend = time()
+        
+        if self.feedback:
+            print('Kernels computed in {:.1f} s'.format(tend - tini))
+
+    
+    def gal_window(self,cosmo,i):
+        bias_binned = cosmo['bias'](self.observables['GC']['zmean'])
+        Wgal = np.array([self.ni_GC[i](z)*cosmo['H_Mpc'](z) for z in cosmo['z']])
+        return Wgal
+
+    def lens_window(self,cosmo,i):
+        Wlens = np.array([cosmo['comov_dist'](z)*self.leff[i][ind] for ind,z in enumerate(cosmo['z'])])
+        return Wlens
+
+    def IA_window(self,cosmo,i):
+        WIA = np.array([self.ni_WL[i](z)*cosmo['H_Mpc'](z) for z in cosmo['z']])
+        return WIA
+
+    def gw_window(self,cosmo,i):
+        Wgw = np.array([cosmo['comov_dist'](z)*self.leff_gw[i][ind] for ind,z in enumerate(cosmo['z'])])
+        return Wgw
+
+    def lens_eff(self,cosmo,z,i):
+
+        zp = np.linspace(z,cosmo['z'][-1],100)
+        leff = trapz(self.ni_WL[i](zp)*(1-cosmo['comov_dist'](z)/cosmo['comov_dist'](zp)),x=zp)
+
+        return leff
+    def gw_eff(self,cosmo,z,i):
+
+        zp = np.linspace(z,cosmo['z'][-1],100)
+        leff_gw = trapz(self.ni_GW[i](zp)*(1-cosmo['comov_dist'](z)/cosmo['comov_dist'](zp)),x=zp)
+
+        return leff_gw
+
+
+
+
+
+
+
+
+
+
+    def get_Pell(self,cosmo,ell,z):
+        
+        kappa = (ell+0.5)/cosmo['comov_dist'](z)
+
+        Pell = {'Pgg': cosmo['bias'](z)**2.*cosmo['Pk_delta'].P(z,kappa),
+                'Pgi': cosmo['bias'](z)*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                'Pig': cosmo['bias'](z)*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                'Pii': cosmo['IA_term'](z)**2*cosmo['Pk_delta'].P(z,kappa)}
+
+        
+        Pell.update({'Pdd': (cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['Pk_delta'].P(z,kappa),
+                         'Pgwgw': (cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['Pk_delta'].P(z,kappa),
+                         'Pdgw': (cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['Pk_delta'].P(z,kappa),
+                         'Pgwd': (cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['Pk_delta'].P(z,kappa),
+                         'Pdi': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pid': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pgwi': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pigw': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['IA_term'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pdg': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['bias'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pgd': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['bias'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pggw': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['bias'](z)*cosmo['Pk_delta'].P(z,kappa),
+                         'Pgwg': np.sqrt(cosmo['Pk_Weyl'].P(z,kappa)/cosmo['Pk_linear'].P(z,kappa))*cosmo['bias'](z)*cosmo['Pk_delta'].P(z,kappa)})
+           
+
+        return Pell
+
+    def get_cls_old(self):
+        
+        cosmo=self.get_cosmo_dict(self.params)
+        integrand = np.array([1/(cosmo['H_Mpc'](z)*cosmo['comov_dist'](z)**2) for z in cosmo['z']]) #prefactor
+
+        pspectra = np.array([[self.get_Pell(cosmo,ell,z) for z in cosmo['z']] for ell in self.ells]) #PS
+        
+        Cls = {}
+        self.get_window()
+        def mysplit(s):
+            head = s.rstrip('0123456789')
+            tail = s[len(head):]
+            return head, tail
+
+        zint = cosmo['z']#np.linspace(cosmo['z'][0],cosmo['z'][-1],10)
+        #print(self.kernels.items())
+        for n1,w1 in self.kernels.items():
+            for n2,w2 in self.kernels.items():
+                obs1,bin1 = mysplit(n1)
+                obs2,bin2 = mysplit(n2)
+                Cls.update({n1+'x'+n2: np.array([trapz([w1[zind]*w2[zind]*integrand[zind]*pspectra[ellind,zind]['P'+obs1+obs2] for zind,z in enumerate(zint)],x=zint) for ellind,ell in enumerate(self.ells)])}) #complete formula for Cls
+
+        #Here build the final Cls in format ObsixObsj where Obs={GC=G,GW=W,WL=L} and i,j are the zbins -> e.g. L1xL1 means CLS evaluated using Lensing in the first bin
+        if 'WL' in self.observables:
+            WLcols = ['L{}xL{}'.format(i,j) for i in range(1,self.Nbins_WL+1) for j in range(i,self.Nbins_WL+1)]
+        if 'GC' in self.observables:
+            GCcols = ['G{}xG{}'.format(i,j) for i in range(1,self.Nbins_GC+1) for j in range(i,self.Nbins_GC+1)]
+        if 'GW' in self.observables:
+            GWcols = ['W{}xW{}'.format(i,j) for i in range(1,self.Nbins_GW+1) for j in range(i,self.Nbins_GW+1)]
+        if 'GC' in self.observables and 'WL' in self.observables:    
+            XCcols = ['G{}xL{}'.format(i,j) for i in range(1,self.Nbins_GC+1) for j in range(1,self.Nbins_WL+1)]
+        if 'GC' in self.observables and 'GW' in self.observables:    
+            GGWcols = ['G{}xW{}'.format(i,j) for i in range(1,self.Nbins_GC+1) for j in range(1,self.Nbins_GW+1)]
+        if 'GW' in self.observables and 'WL' in self.observables:    
+            LGWcols = ['W{}xL{}'.format(i,j) for i in range(1,self.Nbins_WL+1) for j in range(1,self.Nbins_GW+1)]
+        
+        if 'GC' in self.observables and 'WL' in self.observables and 'GW'  in self.observables:
+
+            final_Cls = pd.DataFrame(columns=['ells']+WLcols+XCcols+GCcols+GWcols+GGWcols+LGWcols)
+            
+            
+        if 'GC' not in self.observables and 'WL' in self.observables and 'GW' in self.observables:
+          
+            final_Cls = pd.DataFrame(columns=['ells']+WLcols+GWcols+LGWcols)
+
+        if 'GC'  in self.observables and 'WL' not in self.observables and 'GW' in self.observables:
+          
+            final_Cls = pd.DataFrame(columns=['ells']+GCcols+GWcols+GGWcols)
+            
+            
+        if 'GC' in self.observables and 'WL' in self.observables and 'GW' not in self.observables:
+
+            final_Cls = pd.DataFrame(columns=['ells']+WLcols+GCcols+XCcols)
+            
+        
+        if 'GC' not in self.observables and 'WL' not in self.observables and 'GW' in self.observables:
+            
+            final_Cls = pd.DataFrame(columns=['ells']+GWcols)
+        final_Cls['ells'] = self.ells
+        
+
+########CHECK FOR DIFFERENT GC WL GW BINS
+
+        if 'GC' in self.observables:
+            for bin1 in range(1,self.Nbins_GC+1):
+                for bin2 in range(bin1,self.Nbins_GC+1):
+                    final_Cls['G{}xG{}'.format(bin1,bin2)] = Cls['g{}xg{}'.format(bin1,bin2)]
+                    #print(final_Cls['G{}xG{}'.format(bin1,bin2)])
+
+        if 'WL' in self.observables:
+            for bin1 in range(1,self.Nbins_WL+1):
+                for bin2 in range(bin1,self.Nbins_WL+1):
+                    final_Cls['L{}xL{}'.format(bin1,bin2)] = Cls['d{}xd{}'.format(bin1,bin2)]+Cls['i{}xd{}'.format(bin1,bin2)]+Cls['i{}xi{}'.format(bin1,bin2)]
+          
+
+                    
+                    
+        if 'GW' in self.observables:
+            for bin1 in range(1,self.Nbins_GW+1):
+                for bin2 in range(bin1,self.Nbins_GW+1):
+                    final_Cls['W{}xW{}'.format(bin1,bin2)] = Cls['gw{}xgw{}'.format(bin1,bin2)]
+                    #print(final_Cls['W{}xW{}'.format(bin1,bin2)])
+
+        
+        if 'WL' in self.observables and 'GC' in self.observables:
+            for bin1 in range(1,self.Nbins_GC+1):
+                for bin2 in range(1,self.Nbins_WL+1):
+                    final_Cls['G{}xL{}'.format(bin1,bin2)] = Cls['g{}xd{}'.format(bin1,bin2)]+Cls['g{}xi{}'.format(bin1,bin2)]
+                    final_Cls['L{}xG{}'.format(bin1,bin2)] = Cls['d{}xg{}'.format(bin1,bin2)]+Cls['i{}xg{}'.format(bin1,bin2)]
+                    
+        if 'WL' in self.observables and 'GW' in self.observables:
+            for bin1 in range(1,self.Nbins_WL+1):
+                for bin2 in range(1,self.Nbins_GW+1):
+                    final_Cls['L{}xW{}'.format(bin1,bin2)] = Cls['gw{}xd{}'.format(bin1,bin2)]+Cls['gw{}xi{}'.format(bin1,bin2)]
+                    final_Cls['W{}xL{}'.format(bin1,bin2)] = Cls['d{}xgw{}'.format(bin1,bin2)]+Cls['i{}xgw{}'.format(bin1,bin2)]
+                    
+        if 'GC' in self.observables and 'GW' in self.observables:
+            for bin1 in range(1,self.Nbins_GC+1):
+                for bin2 in range(1,self.Nbins_GW+1):
+                    final_Cls['G{}xW{}'.format(bin1,bin2)] = Cls['g{}xgw{}'.format(bin1,bin2)]
+                    final_Cls['W{}xG{}'.format(bin1,bin2)] = Cls['gw{}xg{}'.format(bin1,bin2)]
+            
+
+
+
+        return final_Cls
+
+
+
+    
