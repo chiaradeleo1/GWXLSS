4150d1fea8e8919d6ca42912e194c3d1f1adde2f
matteo.martinelli <matteomartinelli85@gmail.com>
2024-09-19 15:39:42 +0200

    cleanup of runner to remove hard coded stuff

--- runner.py
+++ runner.py
@@ -1,47 +1,44 @@
-import sys
-import numpy as np
+import sys,os
+import numpy  as np
 import pandas as pd
+import numpy  as np
 
-from cobaya.run import run
-import emcee
-import numpy as np
-
-from cobaya.run import run
 from source_code.likelihood import LSSlike
-from time       import time
-import sys
 
-import warnings
-warnings.filterwarnings('ignore')
-
+from time import time
 from bios import read
 from copy import deepcopy
 
-
 from utils.utils import Suppressor,wbinned_setup
 
-sys.path.append('../')
+#sys.path.append('../')
+from cobaya.run                  import run
 from samplers.samplers_interface import nautilus_interface,run_fisher
+
+import warnings
+warnings.filterwarnings('ignore')
 
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+info     = read(sys.argv[1])
 
+#MMmod:
+#creating output folder if it doesn't exist
+directory = os.path.dirname(os.path.abspath(info['output']))
 
-info = read(sys.argv[1])
-camb_path = '/users/chiaradeleo/Desktop/myeftcamb-main'
-info['likelihood'] =  {'LSS': {'external': LSSlike,
-                               'data_path': './mock_data/EFTflag_1_test_galonly_IApoly',
-                               'debug_mode': False,
-                               'camb_path': camb_path,
-                               'use_noiseless_cls': True}}
-
-
-
-info['force'] = True
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 if list(info['sampler'].keys())[0] == 'mcmc':
+    #MMmod: this needs to be changed to be able to use multiple likelihoods at once
+    likesets = deepcopy(info['likelihood']['LSS'])
+    info['likelihood'] =  {'LSS': {'external': LSSlike,
+                                   'data_path': likesets['data_path'],
+                                   'debug_mode': likesets['debug_mode'],
+                                   'camb_path': likesets['camb_path'],
+                                   'use_noiseless_cls': likesets['use_noiseless_cls']}}
     info['sampler']['mcmc']['covmat'] = 'covariance_matrix.covmat'
     updated_info,sampler = run(info)
 elif list(info['sampler'].keys())[0] == 'nautilus':
