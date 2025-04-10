56490345873f2e0bae9d6cd17e35bf61a257a3f3
Chiara De Leo <chiara.deleo@students.uniroma2.eu>
2025-04-10 16:28:32 +0200

    Scale_cut option from likelihood

--- source_code/likelihood.py
+++ source_code/likelihood.py
@@ -20,6 +20,7 @@ class LSSlike(Likelihood):
         #Hard coded stuff
         
         self.feedback=self.debug_mode
+  
         if self.use_noiseless_cls:
             self.data_Cls     = pd.read_csv(self.data_path+'_Cls_noiseless.dat',sep='\s+',header=0)
         else:
@@ -39,32 +40,63 @@ class LSSlike(Likelihood):
 
 
     def logp(self, **params_values):
-        params = {key: value for key, value in params_values.items() }
-        
-        #self.obs = get_obs(params,self.observables, self.data_ells, self.settings,feedback=self.feedback)
-        theory = get_obs(params,self.observables, self.data_ells, self.settings).Cls
-        
+        params = {key: value for key, value in params_values.items()}
+        theory = get_obs(params, self.observables, self.data_ells, self.settings).Cls
+
         loglike = 0
         like_cols = [col for col in self.data_Cls.columns if col != 'ells']
         ell_diff = []
-        for ind,ell in enumerate(self.data_ells):
-            thvec   = theory.iloc[ind][like_cols].values
-            dtvec   = self.data_Cls.iloc[ind][like_cols].values
-            diffvec = thvec-dtvec 
-            loglike += -0.5*np.dot(diffvec,np.dot(self.invcov[str(int(ell))],diffvec))
-            if self.feedback: 
-                df = pd.DataFrame({'ells': ell}|{col: theory.iloc[ind][col]-self.data_Cls.iloc[ind][col] for col in like_cols},index=[0])
-                ell_diff.append(df)
+        chi2_per_ell = []
+
+        gw_cols = [col for col in like_cols if 'WC' in col or 'WL' in col]
+        
+       
+        for ind, ell in enumerate(self.data_ells):
+
+            thvec = theory.iloc[ind][like_cols].values.copy()
+            dtvec = self.data_Cls.iloc[ind][like_cols].values
+            if ('scale_cut' in self.settings and self.settings['scale_cut'].get('method') == 'ell_cut_like'):
+                if ell > self.settings['scale_cut']['value']:
+                    for i, col in enumerate(like_cols):
+                        if col in gw_cols:
+                            thvec[i] = dtvec[i]
+
+
+            diffvec = thvec - dtvec
+            chi2_val = np.dot(diffvec, np.dot(self.invcov[str(int(ell))], diffvec))
+            loglike += -0.5 * chi2_val
+
+            if self.feedback:
+                diff_dict = {'ells': ell} | {col: thvec[i] - dtvec[i] for i, col in enumerate(like_cols)}
+                ell_diff.append(pd.DataFrame(diff_dict, index=[0]))
+                chi2_per_ell.append((ell, chi2_val))
+
+
 
 
         if self.feedback:
+            print('Plotting')
             plot_df = pd.concat(ell_diff,ignore_index=True)
             plot_df = pd.melt(plot_df,id_vars=['ells'],value_vars=like_cols,var_name='Bin',value_name='value')
             import matplotlib.pyplot as plt
             import seaborn as sb
             plt.figure()
+            plt.title('Theo-data')
+            plt.axvline(x=200, color='gray', linestyle='--')
             sb.lineplot(plot_df,x='ells',y='value',hue='Bin',legend=0)
             plt.xscale('log')
+            plt.show()
+
+            chi2_df = pd.DataFrame(chi2_per_ell, columns=['ell', 'chi2'])
+
+            plt.figure()
+            plt.title('Chi²')
+            plt.axvline(x=200, color='gray', linestyle='--')
+            plt.plot(chi2_df['ell'], chi2_df['chi2'], marker='o', linestyle='-')
+            plt.xscale('log')
+            plt.ylabel('Chi²')
+            plt.xlabel('ell')
+            plt.tight_layout()
             plt.show()
 
         return loglike
